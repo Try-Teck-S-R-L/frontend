4f28d1a2252a8dd4bfed7dc703f6d58368e3a72c
mavl44 <maury.vargasl@gmail.com>
2022-10-06 16:23:09 -0400

    modificando atributos en inerface de preinscripcion y jugador

--- src/app/modules/models/jugador.interface.ts
+++ src/app/modules/models/jugador.interface.ts
@@ -1,3 +1,12 @@
 export interface JugadorI{
-    
+    nombrejugador: string,
+    apellidojugador: string,
+    numerocamiseta: number,
+    categoria: string,
+    edadjugador: number,
+    fotoperfiljugador: File,
+    fotocijugador: File,
+    nacionalidadjugador: string,
+    posicionjugador: string,
+    tallajugador: string
 }
