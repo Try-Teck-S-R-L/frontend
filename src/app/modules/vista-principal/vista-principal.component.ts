cde4f33d9c88eb207a171ee3b401257758c6ea9c
mavl44 <maury.vargasl@gmail.com>
2022-10-24 21:57:57 -0400

    corrigiendo service y ts de ventana principal

--- src/app/modules/vista-principal/vista-principal.component.ts
+++ src/app/modules/vista-principal/vista-principal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoGeneralService } from 'src/app/services/infoGeneral.service';
+import { DelegadoI } from '../models/delegado.interface';
 
 @Component({
   selector: 'app-vista-principal',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaPrincipalComponent implements OnInit {
 
-  constructor() { }
+
+  public $delegado : any;
+  constructor(private http: InfoGeneralService) {}
 
   ngOnInit(): void {
   }
 
+  iniciarSesion($correo: string, $contrasena: string ){
+    /*const delegadoEj = new DelegadoI();
+    delegadoEj.append("correoDelegado", $correo);
+    delegadoEj.append("contraseniaDelegado", $contrasena);*/
+    this.http.iniciarDelegado( $correo, $contrasena ).subscribe((data)=>{
+      console.log("success");
+  });
+  }
 }
--- src/app/services/infoGeneral.service.ts
+++ src/app/services/infoGeneral.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs";
 import { DelegadoI } from "../modules/models/delegado.interface";
+import { ResponseI } from "../modules/models/response.interface";
 
 @Injectable({
     providedIn: 'root'
@@ -11,8 +12,13 @@ import { DelegadoI } from "../modules/models/delegado.interface";
 
     constructor(private http:HttpClient) { }
 
-    getDelegado():Observable<DelegadoI>{
-      return this.http.get<DelegadoI>(this.base_url + "auth/verificar");
+    iniciarDelegado(correo: string, contrasena: string):Observable<ResponseI>{
+        let final_url =this.base_url + "auth/verificar";
+        const delegadoEj = new FormData();
+        //const delegadoEj : DelegadoI;
+        delegadoEj.append("correoDelegado", correo);
+        delegadoEj.append("contraseniaDelegado", contrasena);
+      return this.http.post<ResponseI>(final_url, delegadoEj);
     }
 
   }
