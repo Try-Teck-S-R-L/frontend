3be88b4a0eee0a7a6518368dc028d49975536b56
Paulm009 <poolpos8@gmail.com>
2022-11-21 23:06:03 -0400

    main page y error de preinscripcion

--- src/app/modules/editar-pre-dele/editar-pre-dele.component.ts
+++ src/app/modules/editar-pre-dele/editar-pre-dele.component.ts
@@ -1,7 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+
+import { DelegadoService } from "src/app/services/delegado.service";
+import { ElementolistaService } from "src/app/services/elementolista.service";
+import { EquipoService } from "src/app/services/equipo.service";
+
+import data from "../../../assets/Archivos/data.json";
+import { DelegadoI } from "../models/delegado.interface";
+import { PreinscripcionI } from "../models/preinscripcion.interface";
+import { ResponseI } from "../models/response.interface";
+import {Location} from '@angular/common';
+
+
+
 import { ActivatedRoute } from '@angular/router';
 import { InfoGeneralService } from 'src/app/services/infoGeneral.service';
 import { PreinscripcionService } from 'src/app/services/preinscripcion.service';
+import { error } from "@angular/compiler/src/util";
+import { HostListener } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
+
+interface CountryOption {
+  name: string;
+  value: string;
+}
+
+declare var $: any;
+
 
 @Component({
   selector: 'app-editar-pre-dele',
@@ -9,12 +38,35 @@ import { PreinscripcionService } from 'src/app/services/preinscripcion.service';
   styleUrls: ['./editar-pre-dele.component.css']
 })
 export class EditarPreDeleComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+
+  public paises: CountryOption[] = [];
+  private file: File | null = null;
+  public listaEquipos: any = [];
+  public listaPaises: any = [];
+  public listaCategorias: any = [];
+  public mensajeError: string = '';
+  
+  public delegadoAct: { "nombreDelegado": "string";"apellidoDelegado" : "string" ;"correoDelegado": "string" } | undefined;
+
 
   public idDelegado: string = "";
   public datosPreinscripcion: any = [];
   public fechaValida: number;
 
-  constructor(router: ActivatedRoute,
+  constructor(
+    
+    private formBuilder: FormBuilder,
+    private http: PreinscripcionService,
+    private elemento: ElementolistaService,
+   
+    private delegadoService: DelegadoService,
+    private serviceEquipo: EquipoService,
+    private _location: Location,
+    
+    router: ActivatedRoute,
+
     private generalService: InfoGeneralService,
     private servicePreinscripcion: PreinscripcionService) {
       router.params.subscribe((params) => {
@@ -29,6 +81,137 @@ export class EditarPreDeleComponent implements OnInit {
 
     this.servicePreinscripcion.getPreinscripcionGeneral(this.idDelegado).subscribe((res : any)=>{this.datosPreinscripcion = res, console.log(res)});
     
+
+    this.elemento
+    .getAllCategorias()
+    .subscribe((data) => (this.listaCategorias = data));
+
+  this.elemento.getAllPaises().subscribe((data) => (this.listaPaises = data));
+
+  /*this.serviceEquipo
+    .getAllEquipos()
+    .subscribe((res: any) => (this.listaEquipos = res));*/
+
+  this.delegadoService.getInfoDelegado(this.idDelegado).subscribe((data) => (this.delegadoAct =  data));
+
+  this.registerForm = this.formBuilder.group({
+    nombreDelegado: [],
+    emailDelegado: [],
+    nombreEquipo: ["{nombreEquipo}", [Validators.required, Validators.pattern(/^(\w+\s)*\w+$/)]],
+    paisEquipo: ["", [Validators.required]],
+
+    idCategoria: ["", [Validators.required]],
+    
+    nroComprobante: ["", [Validators.required, Validators.pattern(/^(\w+\s)*\w+$/)]],
+    montoPago: ["350", [Validators.required]],
+    fechaPreinscripcion: ["2022-08-01", [Validators.required]],
+  });
+
+
   }
+
+
+
+
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  // convenience getter for easy access to form fields
+  get form() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.mensajeError = '';
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+
+
+    }
+
+
+
+
+
+    this.http
+      .Preinscripcion({
+        ...this.registerForm.value,
+        voucherPreinscripcion: this.file
+      }, this.idDelegado)
+      
+      /*.subscribe((data) => {
+        let response: ResponseI = data;
+        console.log("File:", this.file);
+        console.log({
+          ...this.registerForm.value,
+          voucherPreinscripcion: this.file,
+        });
+      });*/
+      .subscribe(//data => console.log(data),
+                 error => {
+                  if(error != null){
+                    this.mensajeError = error[0]
+                  }; console.log('errores'),
+                  console.log(error);
+                  if(error == null){
+                    $('#exampleModal').modal('show');
+                  };
+                });
+
+    // display form values on success
+    
+
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+    this.registerForm.clearValidators();
+    this.registerForm.updateValueAndValidity();
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.file = file;
+    }
+  }
+
+  get nombreDelegado(): FormControl {
+    return this.registerForm.get("nombreDelegado") as FormControl;
+  }
+  get nombreEquipo(): FormControl {
+    return this.registerForm.get("nombreEquipo") as FormControl;
+  }
+  get nroComprobante(): FormControl {
+    return this.registerForm.get("nroComprobante") as FormControl;
+  }
+
+
+  regresar(){
+    this.onReset();
+    this._location.back();
+  }
+
 
 }
