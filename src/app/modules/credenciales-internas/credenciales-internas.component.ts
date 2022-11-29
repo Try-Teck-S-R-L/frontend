92d2b02c68d973ce52612177799bcc112d332188
Paulm009 <poolpos8@gmail.com>
2022-11-29 03:51:03 -0400

    credenciales ver y descargar

--- src/app/modules/credenciales-internas/credenciales-internas.component.ts
+++ src/app/modules/credenciales-internas/credenciales-internas.component.ts
@@ -1,34 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from "@angular/router";
+import {QRCodeComponent} from 'angular2-qrcode';
+import { EquipoService } from 'src/app/services/equipo.service';
+import { JugadorService } from 'src/app/services/jugador.service';
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-
-@Component({
+import html2canvas from 'html2canvas';@Component({
   selector: 'app-credenciales-internas',
   templateUrl: './credenciales-internas.component.html',
   styleUrls: ['./credenciales-internas.component.css']
 })
 export class CredencialesInternasComponent implements OnInit {
-  
-  constructor() {
-    
-  }
-  
+  public credencial: FormGroup;
+  public listaJugadores: any = [];
+  public infoEquipo: any = '';
 
-  
+  public idEquipo:string = "";
+  public urlActual: any;
+
+
+  constructor(
+    private jugadorService: JugadorService,
+    private equipoService: EquipoService,
+    router: ActivatedRoute) { 
+      router.params.subscribe((params) => {
+        this.idEquipo = params["id"];
+      });
+      this.urlActual = window.location.href;
+      console.log(this.urlActual);
+      this.credencial = new FormGroup({
+        equipos: new FormControl('',
+          Validators.required)
+      });
+  }
+
   ngOnInit(): void {
-  }
+    this.jugadorService.getAllJugadores(this.idEquipo).subscribe(data => (this.listaJugadores = data, console.log(data)));
+    this.equipoService.getEquipoXid(this.idEquipo).subscribe((data2: any) => (this.infoEquipo = data2));
 
+  }
+  get equipoJugador(){ return this.credencial.value.equipos; }
 
   downloadPDF() {
     // Extraemos el
     const DATA = document.getElementById('credencial');
-    const doc = new jsPDF('p', 'pt', 'a4');
+    const doc = new jsPDF('p', 'pt', 'carta');
     const options = {
       background: 'white',
       scale: 3
     };
+    
     html2canvas(DATA, options).then((canvas) => {
 
       const img = canvas.toDataURL('image/PNG');
@@ -45,24 +66,5 @@ export class CredencialesInternasComponent implements OnInit {
       docResult.save(`${new Date().toISOString()}credencial.pdf`);
     });
   }
-
-
-
- /* PDFMAKE
-  createPdf(){
-
-    const pdfDefinition: any = {
-      content: [
-        {
-          text: 'Hola mundo'
-        }
-      ]
-    }
-
-    const pdf = pdfMake.createPdf(pdfDefinition);
-    pdf.download();
-
-  }*/
-
+  
 }
-
