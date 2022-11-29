e3e6601f4ba6fa618e29a52374d3ede8c779aeac
abrilv4 <102884291+abrilv4@users.noreply.github.com>
2022-11-28 21:28:00 -0400

    menuadmin

---
+++ src/app/modules/listapreadmin/listapreadmin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listapreadmin',
+  templateUrl: './listapreadmin.component.html',
+  styleUrls: ['./listapreadmin.component.css']
+})
+export class ListapreadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
